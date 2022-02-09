b1ddcdf82596b7316b8a91a9da72007083c69d74
Iago Mendes <iagommendes@gmail.com>
2022-02-09 14:22:48 -0300

    updating deploy settings

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const favicon = require('express-favicon');
 const path = require('path');
+const {resolve}= require('path')
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
+
+app.use('/', express.static( resolve( __dirname, './build' ) ) ) 
+app.use('*', express.static( resolve( __dirname, './build' ) ) )
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
